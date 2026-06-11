af7c39352c0715501122103292cdc91a7fbbb9c0
gkpani <gkandhapani@gmail.com>
2026-06-11 12:13:22 +0530

    Added controlled booking form and booking page

--- src/Main.js
+++ src/Main.js
@@ -1,35 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import BookingPage from "./pages/BookingPage";
+
 function Main() {
-    return (
-        <main>
-            <h1>Welcome to Little Lemon</h1>
-
-            <section className="hero">
-                <h1>Reserve a Table</h1>
-                <p>Book your table online and enjoy a delightful dining experience at Little Lemon.
-                   Our cozy atmosphere and delicious menu await you. Reserve now to secure your spot!
-                </p>
-
-            </section>
-
-            <section className="Booking-Section">
-                <div className="card">
-                    Date
-                </div>
-
-                <div className="card">
-                    Time
-                </div>
-
-                <div className="card">
-                    Guests
-                </div>
-
-                <div className="card">
-                    Occasion
-                </div>
-            </section>
-        </main>
-    );
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/booking" element={<BookingPage />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default Main;
--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -1,6 +1,21 @@
+import BookingForm from "../components/BookingForm";
+
 function BookingPage() {
   return (
-    <h1>Reserve a Table</h1>
+    <main>
+      <h1>Reserve a Table</h1>
+
+      <p>
+        Please complete the form below to reserve
+        your table at Little Lemon.
+      </p>
+
+      <BookingForm />
+
+      <p>
+        We look forward to serving you.
+      </p>
+    </main>
   );
 }
 
